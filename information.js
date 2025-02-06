e7fe8497b51046d4382c35620409e579b2c59dda
anna-dahlberg <anna.dahlberg@live.se>
2025-02-06 13:32:36 +0100

    successfully created request against API with access token

--- information.js
+++ information.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import qs from 'qs';
 import dotenv from 'dotenv';
 
+let stopName = "9021014004790000";
+let platform = "A";
+
 let data = qs.stringify({
   'grant_type': 'client_credentials',
   'client_id': process.env.VASTTRAFIK_ID,
@@ -9,7 +12,7 @@ let data = qs.stringify({
   'scope': '' 
 });
 
-let config = {
+let configAccessToken = {
   method: 'post',
   maxBodyLength: Infinity,
   url: 'https://ext-api.vasttrafik.se/token',
@@ -20,12 +23,35 @@ let config = {
   data : data
 };
 
-axios.request(config)
+
+
+axios.request(configAccessToken)
 .then((response) => {
  let accessResponse = response.data;
  let accessToken = accessResponse["access_token"];
-  
+
+ let configGetAPI = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: `https://ext-api.vasttrafik.se/pr/v4/stop-areas/${stopName}/departures?platforms=${platform}&timeSpanInMinutes=1439&maxDeparturesPerLineAndDirection=3&limit=3&offset=0&includeOccupancy=false`,
+  headers: { 
+    'Authorization': `Bearer ${accessToken}`, 
+    'Cookie': 'TS0153fc3c=0166f7c4487879013886c34e0d6cfcd1d32e206287b33a7b993ec0d5da52bd258df906419314d846ff971e2fe99c7f1b3fdf3e0c49; route=85bec1da72835c0'
+  },
+  data : data
+};
+ axios.request(configGetAPI)
+ .then((response) => {
+   console.log(JSON.stringify(response.data));
+ })
+ .catch((error) => {
+   console.log(error);
+ });
+
 })
 .catch((error) => {
   console.log(error);
 });
+
+
+
