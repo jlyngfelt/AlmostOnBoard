3d5f2494f6d1c41c469b4972296375c17a93d60d
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-02-11 14:04:22 +0100

    test apiurl

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchData() {
     try {
-        const response = await fetch("https://https://almost-on-board.vercel.app/data");
+        const response = await fetch("http://almost-on-board.vercel.app/data");
         const data = await response.json();
         
         // Uppdatera HTML med API-datan
