48f6fdedefbf89c9715ae84225adc522d3a3347b
anna-dahlberg <anna.dahlberg@live.se>
2025-02-11 13:28:31 +0100

    attempted to create env variable for url

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { fetchVasttrafikData } from './information.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import process from 'process';
+
 
 const app = express();
 const port = 4000;
 const url = 'https://ext-api.vasttrafik.se/pr/v4';
+
+dotenv.config();
+
       
 app.use(cors());
 
