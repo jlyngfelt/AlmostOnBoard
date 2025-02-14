eebe14a5a072dda97f13e0f9de70d484d8b9c278
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2025-02-14 14:19:24 +0100

    frontend variables displyed

--- public/script.js
+++ public/script.js
@@ -7,10 +7,10 @@ async function fetchData() {
       endpoint = `/data/${localToken}`;
     }
 
-    // const response = await fetch(`http://localhost:4000${endpoint}`);
+    const response = await fetch(`http://localhost:4000${endpoint}`);
 
     // Link to use on deploy:
-    const response = await fetch(`https://almost-on-board.vercel.app${endpoint}`);
+    // const response = await fetch(`https://almost-on-board.vercel.app${endpoint}`);
 
     const result = await response.json();
 
@@ -23,6 +23,17 @@ async function fetchData() {
 
     document.getElementById("shortName").textContent = data.shortName;
     document.getElementById("shortDirection").textContent = data.shortDirection;
+    document.getElementById("stopPointName").textContent = data.stopPointName;
+    document.getElementById("transportMode").src = "/img/" + data.transportMode + ".png";
+    document.getElementById("isCancelled").textContent = data.isCancelled;
+
+    const estTime = new Date(data.estTime); // Omvandla sträng till Date-objekt
+    const now = new Date();
+    const diffInMinutes = Math.max(0, Math.round((estTime - now) / 1000 / 60));
+
+    document.getElementById("estTime").textContent = diffInMinutes;
+
+
   } catch (error) {
     console.error("Error fetching data:", error);
     localStorage.removeItem("accesstoken");
