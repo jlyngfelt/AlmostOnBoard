74066168b41a234399295808a4a4d9c1f8f2e3b1
anna-dahlberg <anna.dahlberg@live.se>
2025-02-20 14:07:37 +0100

    Modified search API to only show stop points

--- src/search.js
+++ src/search.js
@@ -4,7 +4,7 @@ export const searchForStopPoint = async (accessToken, searchText) => {
     try {
         const response = await axios({
             method: 'get',
-            url: `https://ext-api.vasttrafik.se/pr/v4/locations/by-text?q=${searchText}&limit=5`,
+            url: `https://ext-api.vasttrafik.se/pr/v4/locations/by-text?q=${searchText}&types=stoparea&limit=5&`,
             headers: {
                 'Authorization': `Bearer ${accessToken}`
             }
